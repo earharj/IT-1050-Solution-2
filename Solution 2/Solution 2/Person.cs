0737b942788db3ebdc61d2406ad0bf61f2d8261c
Jackie Earhart <jmricha13@yahoo.com>
2016-10-03 22:51:01 -0400

    Solution 2

---
+++ Solution 2/Solution 2/Person.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace Solution_2
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+                          
+        
+        public void GatherInformation()
+        {
+            System.Console.Write("Enter your first name: ");
+            FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter your last name: ");
+            LastName = System.Console.ReadLine();
+
+            System.Console.Write("Enter your age: ");
+            Age = int.Parse(System.Console.ReadLine());
+            Person.SumOfAllAges += Age;
+                                                
+            Spouse = new Person();
+
+            System.Console.Write("Enter your Spouse's first name: ");
+            Spouse.FirstName = System.Console.ReadLine();
+
+            Spouse.LastName = LastName;
+
+            System.Console.Write("Enter your Spouse's age: ");
+            Spouse.Age = int.Parse(System.Console.ReadLine());
+            Person.SumOfAllAges += Age;
+
+          }
+
+                      
+
+        public string GetFullName()
+        {
+            string FullName = LastName + "," + FirstName;
+            return FullName;
+
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(GetFullName() + "  " + "(" + Age + ")");
+        }
+
+        
+        public static double SumOfAllAges;
+        
+    
+        
+        
+         
+        
+       
+    }
+}
---
+++ Solution 2/Solution 2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Solution_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person();
+                        
+            System.Console.WriteLine("[ Enter Information for p1 ]");
+            
+            p1.GatherInformation();
+
+            System.Console.WriteLine("[ Enter Information for p2 ]");
+
+            p2.GatherInformation();                              
+
+            System.Console.WriteLine("[ Printing Results ] ");
+
+            p1.PrintNameAndAge();
+            p1.Spouse.PrintNameAndAge();
+            p2.PrintNameAndAge();
+            p2.Spouse.PrintNameAndAge();
+                        
+            double AverageAge = Person.SumOfAllAges / 4;
+            System.Console.WriteLine("Average Age =" + AverageAge);                       
+            System.Console.WriteLine("Press any key to continue...");
+
+            System.Console.ReadKey();
+
+            
+        }
+    }
+}
